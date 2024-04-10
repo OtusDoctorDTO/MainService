3c9976079ecc3baae0b90c9a8b8aa0a92de9de98
KozyrevaSvetlana <lanako0507@gmail.com>
2024-04-10 08:45:14 +0300

    настройки

---
+++ Domain.Entities/Configs/IApplicationConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Configs
+{
+    public interface IApplicationConfig
+    {
+        RabbitMqConfig BusConfig { get; set; }
+        string DoctorHost { get; set; }
+    }
+}
