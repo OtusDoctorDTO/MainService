eab4f70933dab5843c6f7317a96e78b68c587c53
olya_nick <olya17nikolbas@gmail.com>
2024-05-26 01:36:30 +0300

    Добавлена модель для View

--- MainServiceWebApi/Controllers/PatientController.cs
+++ MainServiceWebApi/Controllers/PatientController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelpersDTO.Authentication.DTO.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MainServiceWebApi.Controllers
 {
@@ -8,5 +10,28 @@ namespace MainServiceWebApi.Controllers
         {
             return View();
         }
+
+        //private readonly IPatientServiceClient _patientServiceClient;
+
+        //public PatientController(IPatientServiceClient patientServiceClient)
+        //{
+        //    _patientServiceClient = patientServiceClient;
+        //}
+
+
+        //[HttpGet]
+        //[Authorize]
+        //public async Task<IActionResult> Profile()
+        //{
+        //    var token = HttpContext.Session.GetString("Token");
+        //    var user = await _patientServiceClient.GetProfileAsync(token);
+        //    return View(user);
+        //}
+
+        //public IActionResult Logout()
+        //{
+        //    HttpContext.Session.Remove("Token");
+        //    return RedirectToAction("Login");
+        //}
     }
 }
--- MainServiceWebApi/Models/PatientViewModel.cs
+++ MainServiceWebApi/Models/PatientViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class PatientViewModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
