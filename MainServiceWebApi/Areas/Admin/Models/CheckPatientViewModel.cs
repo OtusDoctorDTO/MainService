58188a78cdd4c8011aa6b5d421dbbb5edb12198a
KozyrevaSvetlana <lanako0507@gmail.com>
2024-06-02 11:52:17 +0300

    паспортные данные - предварительная версия

---
+++ MainServiceWebApi/Areas/Admin/Models/CheckPatientViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MainServiceWebApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MainServiceWebApi.Areas.Admin.Models
+{
+    public class CheckPatientViewModel
+    {
+        //добавить данные заполненные из записи ко врачу
+        public Guid? Id { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        /// <summary>
+        /// Тип документа (по умолчанию - паспорт)
+        /// </summary>
+        public int DocumentType { get; set; } = 1;
+        public PassportViewModel? Passport { get; set; } = new();
+
+        /// <summary>
+        /// Пол
+        /// </summary>
+        [Required]
+        [Range(1, 2)]
+        public int? Gender { get; set; }
+    }
+}
---
+++ MainServiceWebApi/Models/PassportViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainServiceWebApi.Models
+{
+    public class PassportViewModel
+    {
+        /// <summary>
+        /// Серия
+        /// </summary>
+        [Required(ErrorMessage = "Не указана серия")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "Длина строки должна быть 4 символа")]
+        public string? Series { get; set; }
+
+        /// <summary>
+        /// Номер
+        /// </summary>
+        [Required(ErrorMessage = "Не указан номер")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Длина строки должна быть 6 символа")]
+        public string? Number { get; set; }
+
+        /// <summary>
+        /// Дата выдачи
+        /// </summary>
+        [Required(ErrorMessage = "Не указана дата выдачи")]
+        public DateTime? IssueDate { get; set; }
+
+        /// <summary>
+        /// Код подразделения
+        /// </summary>
+        [Required(ErrorMessage = "Не указан код подразделения")]
+        [RegularExpression(@"^\d{3}\-\d{3}$", ErrorMessage = "Некорректный формат кода подразделения")]
+        public string? SubdivisionCode { get; set; }
+
+        /// <summary>
+        /// Кем выдан
+        /// </summary>
+        [Required(ErrorMessage = "Не указано кем выдано")]
+        public string? IssuedBy { get; set; }
+        
+    }
+}
