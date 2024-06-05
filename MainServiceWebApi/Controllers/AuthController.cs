06388c8b11bcd0dd0dbe7830cccb999c54805369
olya_nick <olya17nikolbas@gmail.com>
2024-06-05 19:52:28 +0300

    Добавление пользователя через сервис пациента

--- MainServiceWebApi/Controllers/AuthController.cs
+++ MainServiceWebApi/Controllers/AuthController.cs
@@ -115,12 +115,15 @@ namespace MainServiceWebApi.Controllers
                     var userId = registerResponce.UserId;
                     if (userId != null)
                     {
-                        await _patientService.AddPatientAsync(new PatientDTO
+                        var patientDto = new PatientDTO
                         {
-                            UserId = userId,
-                            IsNew = true,
+                            UserId = userId.Value,
+                            FirstName = null,
+                            LastName = null,
                             Phone = model.Phone,
-                        });
+                            IsNew = true
+                        };
+                        await _patientService.AddPatientAsync(patientDto);
                     }
 
                     if (!string.IsNullOrEmpty(model.ReturnUrl))
